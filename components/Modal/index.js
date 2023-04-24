1ba7a287f05deabd4ff022321ab1158ae290e595
Hunga9k50doker <19520101@gm.uit.edu.vn>
2023-04-24 18:59:10 +0700

    update

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -44,6 +44,7 @@ export default function ModalInfoCustomer({ isOpen, setIsOpen, onSubmit, onChang
                           className="w-full rounded-md shadow-sm ring-1 ring-inset ring-gray-300 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                           placeholder="your name"
                           name="username"
+                          required
                           onChange={onChange}
                         />
                         <input
@@ -71,6 +72,7 @@ export default function ModalInfoCustomer({ isOpen, setIsOpen, onSubmit, onChang
                           className="w-full rounded-md shadow-sm ring-1 ring-inset ring-gray-300 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                           placeholder="address"
                           name="address"
+                          required
                           onChange={onChange}
                         />
                       </div>
